8b7d84e8a5e159b40ebc1bd7bf41629143d5a368
Nhatty7 <nayopia777@gmail.com>
2022-06-06 18:30:38 -0700

    Edit task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,10 +18,13 @@ int main(void)
 			putchar(i);
 			putchar(j);
 
-			if (i != '8' && j != '9')
+			if (i != '8')
 			{
-				putchar(',');
-				putchar(' ');
+				if (j != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
