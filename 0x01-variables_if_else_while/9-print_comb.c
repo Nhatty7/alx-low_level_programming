cd85f7ddab5a12bfbf0d05f4a3f8f6f665d37eb9
Nhatty7 <nayopia777@gmail.com>
2022-06-06 17:44:42 -0700

    Add task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+# include <stdlib.h>
+# include <time.h>
+# include <stdio.h>
+
+/**
+ * main -Prints comma separated numbers 0 to 9
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int az;
+
+	for (az = '0'; az <= '9'; az++)
+	{
+		putchar(az);
+
+		if (az != 9)
+		{
+			putchar(',');
+			putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
