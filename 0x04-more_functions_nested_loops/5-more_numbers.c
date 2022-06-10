74a99360336846c11f1f99dc3cad58a9e53f8358
Nhatty7 <nayopia777@gmail.com>
2022-06-09 19:44:45 -0700

    Add 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 0-9 10 times
+ * return: nothing
+ */
+
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y > 9)
+			{
+				_putchar((y / 10) + '0');
+				_putchar((y % 10) + '0');
+			}
+			else
+			{
+				_putchar(y + '0');
+			}
+		}
+
+		_putchar('\n');
+	}
+}
