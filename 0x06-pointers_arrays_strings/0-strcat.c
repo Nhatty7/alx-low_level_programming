3db3f13ed13af28d70af441796ee34454b546fbc
Nhatty7 <nayopia777@gmail.com>
2022-06-16 06:44:41 -0700

    Add some c h and md files

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - concatenetes two strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int len = 0;
+	int i;
+
+	while (dest[len])
+	{
+		len++;
+	}
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[len] = src[i];
+		len++;
+	}
+
+	dest[len] = '\0';
+	return(dest);
+}
