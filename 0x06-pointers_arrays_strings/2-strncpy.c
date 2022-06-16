98b59ef683672753755a971aee6f46a4dec51260
Nhatty7 <nayopia777@gmail.com>
2022-06-16 07:01:11 -0700

    Add task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy string
+ * @dest: destination string
+ * @src: source string
+ * Return: char value
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a = 0;
+	int b = 0;
+
+	while (src[b])
+	{
+		b++;
+	}
+
+	while (a < n && src[a])
+	{
+		dest[a] = src[a];
+		a++;
+	}
+
+	while (a < n)
+	{
+		dest[a] = '\0';
+		a++;
+	}
+
+	return (dest);
+}
