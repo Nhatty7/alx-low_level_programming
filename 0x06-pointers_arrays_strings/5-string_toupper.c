5fcd28536d742c742514cd8120aeae5abf0fb83b
Nhatty7 <nayopia777@gmail.com>
2022-09-21 17:36:52 -0700

    Add 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * string_toupper - Changes all lowercase letters to uppercase
+  * @p: The string will be modified
+  *
+  * Return: char var
+  */
+char *string_toupper(char *p)
+{
+	int i;
+
+	for (i = 0; p[i] != '\0'; i++)
+	{
+		if (p[i] >= 97 && p[i] <= 122)
+		{
+			p[i] -= 32;
+		}
+		else
+		{
+			continue;
+		}
+	}
+
+	return(p);
+}
