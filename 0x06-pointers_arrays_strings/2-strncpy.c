08ecf04c52f3a1d7c4877936e726b60b3917c853
Nhatty7 <nayopia777@gmail.com>
2022-09-21 17:23:01 -0700

    Add 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,28 +6,41 @@
  * @src: source string
  * Return: char value
  */
-
 char *_strncpy(char *dest, char *src, int n)
 {
-	int a = 0;
-	int b = 0;
-
-	while (src[b])
+	int i;
+	int countS = 0;
+	int countD = 0;
+	
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		b++;
+		countS++;
 	}
-
-	while (a < n && src[a])
+	
+	for (i = 0; dest[i] != '\0'; i++)
 	{
-		dest[a] = src[a];
-		a++;
+		countD++;
 	}
-
-	while (a < n)
+	
+	if (n <= countS)
 	{
-		dest[a] = '\0';
-		a++;
+		for (i = 0; i < n && i < countD; i++)
+		{
+			dest[i] = src[i];
+		}
 	}
-
+	else
+	{
+		for (i = 0; src[i] != '\0' && i < countD; i++)
+		{
+			dest[i] = src[i];
+		}
+		while (i < n)
+		{
+			dest[i] = '\0';
+			i++;
+		}
+	}
+	
 	return (dest);
 }
