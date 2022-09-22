7842052bf31b8720220baa4669721deab49d2b9e
Nhatty7 <nayopia777@gmail.com>
2022-09-21 17:19:10 -0700

    Add 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,19 +7,39 @@
  * Return: char pointer
  */
 
-char *_strncat(cahr *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
-	int x, y;
-
-	for (x = 0; dest[x] != '\0'; x++)
+	int i;
+	int countD = 0;
+	int countS = 0;
+	
+	for (i = 0; dest[i] != '\0'; i++)
 	{
-		;
+		countD++;
 	}
-
-	for (y = 0; src[y] != '\0' && n > 0; y++, n--, x++)
+	
+	for (i = 0; src[i] != '\0'; i++) // w o r
 	{
-		dest[x] = src[y];
+		countS++; // 1 2 3
 	}
-
+	
+	if (n <= countS)
+	{
+		for (i = 0; i < n; i++)
+		{
+			dest[countD] = src[i];
+			countD++;
+		}
+	}
+	else
+	{
+		for (i = 0; i < countS; i++)
+		{
+			dest[countD] = src[i];
+			countD++;
+		}
+		dest[countD] = '\0';
+	}
+	
 	return (dest);
 }
